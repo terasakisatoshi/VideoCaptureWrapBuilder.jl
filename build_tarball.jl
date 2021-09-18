e33e73e4e6607f361e310763ae61b605f606d0a9
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-09-19 04:16:19 +0900

    update

--- build_tarball.jl
+++ build_tarball.jl
@@ -1,9 +1,10 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
+using Base.BinaryPlatforms
 using BinaryBuilder, Pkg
 
 name = "VideoCaptureWrap"
-version = v"0.8.0"
+version = v"0.9.0"
 
 # copy LICENSE file
 cp("LICENSE", joinpath("src", "LICENSE"), force=true)
@@ -35,19 +36,19 @@ install_license projectname/LICENSE
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-   Linux(:i686, libc=:glibc),
-   Linux(:x86_64, libc=:glibc),
-   Linux(:aarch64, libc=:glibc),
-   Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Platform("i686", "linux", libc="glibc"),
+    Platform("x86_64", "linux", libc="glibc"),
+    Platform("aarch64", "linux"; libc="glibc"),
+    Platform("armv7l", "linux"; libc="glibc"),
    #Linux(:powerpc64le, libc=:glibc), <- OpenCVQt_jll does not provide for this platform
    #Linux(:i686, libc=:musl), <- OpenCV_jll does not provide for this platform
    #Linux(:x86_64, libc=:musl), <- fails
-   Linux(:aarch64, libc=:musl),
-   Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-   MacOS(:x86_64),
-   #FreeBSD(:x86_64), <- fails,
-   Windows(:i686),
-   Windows(:x86_64),
+    Platform("aarch64", "linux"; libc="musl"),
+    Platform("armv7l", "linux"; libc="musl"),
+    Platform("x86_64", "macos"),
+    #FreeBSD(:x86_64), <- fails,
+    Platform("x86_64", "windows"), 
+    Platform("i686", "windows"),
 ] |> expand_cxxstring_abis
 
 # The products that we will ensure are always built
@@ -58,9 +59,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="libcxxwrap_julia_jll", rev="libcxxwrap_julia-v0.8.5+0")),
+    Dependency(PackageSpec(name="libcxxwrap_julia_jll", rev="libcxxwrap_julia-v0.8.7+0")),
     Dependency(PackageSpec(; name="OpenCVQt_jll", uuid="2e4a2372-4a7c-5bb9-ae2d-b1938f154ce2", url="https://github.com/terasakisatoshi/OpenCVQt_jll.jl", rev="OpenCVQt-v0.7.1+0")),
-    BuildDependency(PackageSpec(name="libjulia_jll", version=v"1.5.1")),
+    BuildDependency(PackageSpec(name="libjulia_jll", version=v"1.6.0")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
